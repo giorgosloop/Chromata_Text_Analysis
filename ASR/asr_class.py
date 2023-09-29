ce8c43637996674e2d4bb9fe5b79887a9e282b2c
DoraPist <dora.pist@gmail.com>
2023-09-29 16:08:42 +0300

    Update asr_class.py

--- ASR/asr_class.py
+++ ASR/asr_class.py
@@ -1,1 +1,14 @@
+import whisper
 
+class ASR():
+
+    def __init__(self):
+
+        self.model = whisper.load_model("large")
+        self.script = ''
+
+    def run_ASR(self, s):
+
+        self.script = self.model.transcribe(s)['text']
+
+        return self.script
